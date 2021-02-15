5e6ec391c1327d8e58572c2d6f9d4bf33f086805
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-02-15 19:13:46 +0700

    lab3_4 ตัวอย่างตรงแต่ InQ

--- lab3_4.c
+++ lab3_4.c
@@ -3,37 +3,15 @@ int main()
 {
     long long number;
     scanf("%lld",&number);
-    if(number==1)
+    long long save=number;
+    for(int i=2;i<=number;i++)
     {
-        printf("1");
-    }
-    else if(number==0)
-    {
-        printf("0");
-    }
-    else if(number%2==0)
-    {
-        if(number%5!=0&&number<10)
+        while (save%i==0)
         {
-            printf("%lld",number-1);
-        }
-        else
-        {
-            number=number-2;
-            printf("%lld",number-1);
+            save=save-1;
         }
     }
-    else
-    {
-        int save=number/2;
-        for(int i=3;i<=save;i++)
-        {
-            if(number%i!=0)
-            {
-                printf("%lld",number);
-                break;
-            }
-        }
-    }
+    printf("%lld",save+1);
+    
     return 0;
 }
