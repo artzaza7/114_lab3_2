9484c8cb5c7ed0251c78bb19befe6288d72d7ed3
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-02-15 19:58:48 +0700

    lab3_4 ยังไม่ได้

--- lab3_4.c
+++ lab3_4.c
@@ -1,10 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    long long number;
+    long long number,k;
     scanf("%lld",&number);
     long long save=number;
-    int flag=0;
-    
+    if(number==1||number==0||number==2)
+    {
+        printf("%lld",number);
+    }
+    for(long long i=2;i<=number;i++)
+    {
+        while ((save%i)==0)
+        {
+            save=save-1;
+        }
+    }
+    for(long long i=2;i<=save+1;i++)
+    {
+        while ((save%i)==0)
+        {
+            save=save-1;
+        }
+    }
+    printf("%lld",save+1);
     return 0;
 }
