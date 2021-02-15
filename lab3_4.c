38f7c4061316a7ce605af98b73ea4ba216c75660
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-02-15 19:18:36 +0700

    lab3_4 เพิ่มเงื่อนไข

--- lab3_4.c
+++ lab3_4.c
@@ -4,14 +4,20 @@ int main()
     long long number;
     scanf("%lld",&number);
     long long save=number;
-    for(int i=2;i<=number;i++)
+    if(save==0||save==1)
+    {
+        printf("%lld",save);
+    }
+    for(long long i=2;i<=number&&number!=0&&number!=1;i++)
     {
         while (save%i==0)
         {
             save=save-1;
         }
     }
-    printf("%lld",save+1);
-    
+    if(save!=0&&save!=1)
+    {
+        printf("%lld",save+1);
+    }
     return 0;
 }
