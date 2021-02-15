57ec16af290cf2c6af4cb4f717dfb8bc3b66a478
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-02-15 20:08:46 +0700

    lab3_4 บางกรณีไม่ได้

--- lab3_4.c
+++ lab3_4.c
@@ -4,24 +4,26 @@ int main()
     long long number,k;
     scanf("%lld",&number);
     long long save=number;
+    int flag=0;
     if(number==1||number==0||number==2)
     {
         printf("%lld",number);
     }
     for(long long i=2;i<=number;i++)
     {
-        while ((save%i)==0)
+        if(save%i==0)
         {
             save=save-1;
+            flag=1;
         }
     }
-    for(long long i=2;i<=save+1;i++)
+    if(flag==1)
     {
-        while ((save+1%i)==0)
-        {
-            save=save-1;
-        }
+        printf("%lld",save+1);
     }
-    printf("%lld",save+1);
+    else
+    {
+        printf("%lld",number);
+    }
     return 0;
 }
