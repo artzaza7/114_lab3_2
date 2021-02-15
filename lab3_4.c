86df0c51c8ac959b6ee38de34d9799f34200cb54
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-02-15 19:43:38 +0700

    lab3_4 Reset

--- lab3_4.c
+++ lab3_4.c
@@ -4,25 +4,7 @@ int main()
     long long number;
     scanf("%lld",&number);
     long long save=number;
-    if(save==0||save==1)
-    {
-        printf("%lld",save);
-    }
-    for(long long i=2;i<=number&&number!=0&&number!=1;i++)
-    {
-        if(save==i)
-        {
-            printf("%lld",save);
-            break;
-        }
-        while (save%i==0)
-        {
-            save=save-1;
-        }
-    }
-    if(save!=0&&save!=1&&save!=2)
-    {
-        printf("%lld",save+1);
-    }
+    int flag=0;
+    
     return 0;
 }
